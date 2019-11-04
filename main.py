0237f29d386cea48e1ef4fea08d4c0234401ac5b
Serge <kims230599@gmail.com>
2019-11-04 12:41:53 +0300

    requirements

---
+++ main.py
@@ -0,0 +1,33 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+print(th.__version__)
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.channels_x2 = nn.Conv2d(3, 6, 1)
+    def forward(self, x):
+        x = self.channels_x2(x)
+        return x
+
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(0):
+    for i, data in enumerate(trainLoader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
