7ba175bfb5be0683bbec69bafaabbe935dbba881
Ivan Lazunin <code.wibbn@gmail.com>
2019-11-05 14:14:32 +0300

    feature init commit

--- helsinki.py
+++ helsinki.py
@@ -2,10 +2,10 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-def get_img(id):
+def get_img(id, img_num):
     url = 'https://www.hel-looks.com/big-photos/{}.jpg'.format(id)
     r = requests.get(url, allow_redirects=True)
-    open('outdoor/img_{}.jpg'.format(id), 'wb').write(r.content)
+    open('outdoor/{}.jpg'.format(img_num), 'wb').write(r.content)
 
 def get_dataset():
     os.mkdir('outdoor')
@@ -14,8 +14,8 @@ def get_dataset():
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     links = soup.find_all('a', {'class': 'v'})
-    for link in links:
+    for link, img_num in zip(links, range(len(links))):
         img_id = link.get('href')[1:]
-        get_img(img_id)
+        get_img(img_id, img_num)
 
 get_dataset()
