a3743bbcb22a78484959e549944015e15898468e
Ivan Lazunin <code.wibbn@gmail.com>
2019-11-08 01:17:33 +0300

    Create Collate function for Dataloader and change batch format

--- Image_DataSet.py
+++ Image_DataSet.py
@@ -1,30 +1,25 @@
 import os
-import torchvision
+import torchvision as thv
 from PIL import Image
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
-import torch
+import torch as th
 
 class ImageDataSet(Dataset):
-    def __init__(self, class_='indoors', len_=15305):
-        self.len = len_
-        self.path = ''
-        if class_ == 'indoor':
-            self.path = os.getcwd() + '/data/indoor'
-        elif class_ == 'outdoor':
-            self.path = os.getcwd() + '/data/outdoor'
-        else:
-            raise Exception()
+    def __init__(self, _len=1000):
+        self.len = _len
+        self.indoor_path = './data/indoor'
+        self.outdoor_path = './data/indoor'
         self.permutation = np.random.permutation(self.len)
 
     def __len__(self):
         return self.len
 
-    def __getitem__(self, id):
-        img = Image.open(self.path + '/' + str(self.permutation[id]) + '.jpg')
+    def __get_img(self, id, path):
+        img = Image.open(path + '/' + str(self.permutation[id]) + '.jpg')
         img = img.convert('RGB') #Sometimes cv2.imread reads RGBA as RGB and after 'fro raw to requred there are still some JPEG'
 
-        crop = torchvision.transforms.RandomCrop(128)
+        crop = thv.transforms.RandomCrop(128)
         croped_image = np.array(crop(img))
 
         if min(croped_image.shape[0], croped_image.shape[1]) < 128:
@@ -34,7 +29,15 @@ class ImageDataSet(Dataset):
         #print(transposed_image.shape)
         if (transposed_image.shape[0] == 4):
             raise Exception('image ' + str(self.permutation[id]) + 'is jpeg, but jpg is required')
-        return torch.Tensor((255 - transposed_image) / 255)
+        return th.Tensor((255 - transposed_image) / 255)
+
+    def __getitem__(self, id):
+        indoor_item = self.__get_img(id, self.indoor_path)
+        outdoor_item = self.__get_img(id, self.outdoor_path)
+
+        item = th.stack([indoor_item, outdoor_item])
+        # print(item.shape)
+        return item
 
 '''
 loader_indoors = ImageDataSet('indoor', 15305)
--- train.py
+++ train.py
@@ -5,22 +5,17 @@ import torch.nn.functional as F
 import torch.optim as optim
 from model import NetArticle
 import Image_DataSet as dtst
+import Collate as clt
+
 ALPHA = 0.9
 
-
-def train(train_loader_indoor, train_loader_outdoor, model, criterion, optimizer, epochs=10, alpha=ALPHA):
+def train(train_loader, lable, model, criterion, optimizer, epochs=10, alpha=ALPHA):
     losses = []
     model.train()
     for epoch in range(epochs):
         losses = []
-        for i, batch in enumerate(zip(train_loader_indoor, train_loader_outdoor)):
-            batch_indoor, batch_outdoor = batch  # batch.shape ((bs, 3, n, n), (bs, 3, n, n)) ((bs, 3, n, n), (bs, 3, n, n))
-            #print(len(batch), type(batch[0]), type(batch[1]), batch_indoor.shape, batch_outdoor.shape)
-            features_indoor, ground_truth_indoor = batch_indoor, batch_indoor
-            #   print(features_indoor)
-            features_outdoor, ground_truth_outdoor = batch_outdoor, batch_outdoor
-            features = alpha * features_indoor + (1 - alpha) * features_outdoor
-            ground_truth = th.Tensor(np.concatenate((ground_truth_indoor, ground_truth_outdoor), axis=1))
+        for i, (features, ground_truth) in enumerate(zip(train_loader, lable)):
+            # batch.shape ((bs, 3, n, n), (bs, 3, n, n)) ((bs, 6, n, n), (bs, 6, n, n))
             optimizer.zero_grad()
             predicts = model(features)
             loss = criterion(predicts, ground_truth)
@@ -32,16 +27,10 @@ def train(train_loader_indoor, train_loader_outdoor, model, criterion, optimizer
     return losses
 
 
-def test(test_loader, model, criterion, alpha=ALPHA):
+def test(test_loader, lable, model, criterion, alpha=ALPHA):
     losses = []
     model.eval()
-    for i, batch in enumerate(zip(test_loader_indoor, test_loader_outdoor)):
-        batch_indoor, batch_outdoor = batch
-        features_indoor, ground_truth_indoor = batch_indoor
-        features_outdoor, ground_truth_outdoor = batch_outdoor
-        features = alpha * features_indoor + (1 - alpha) * features_outdoor
-        ground_truth = np.concatenate((ground_truth_indoor, ground_truth_outdoor), axis=1)
-
+    for i, (features, ground_truth) in enumerate(zip(test_loader, lable)):
         predicts = model(features)
         loss = criterion(predicts, ground_truth)
         losses.append(loss.item())
@@ -50,14 +39,17 @@ def test(test_loader, model, criterion, alpha=ALPHA):
 
 
 def main():
-    n, m = 50, 50
-    train_loader_outdoor = th.utils.data.DataLoader(dtst.ImageDataSet('outdoor', n), batch_size=4)
-    train_loader_indoor = th.utils.data.DataLoader(dtst.ImageDataSet('indoor', m), batch_size=4)
+    n = 50
+
+    data = dtst.ImageDataSet(n)
+
+    train_loader = th.utils.data.DataLoader(data, batch_size=4, collate_fn=clt.CollateFn(alpha=ALPHA))
+    lable_loader = th.utils.data.DataLoader(data, batch_size=4, collate_fn=clt.CollateFn(alpha=ALPHA, lable=True))
 
     net = NetArticle()
     criterion = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
-    losses = train(train_loader_indoor, train_loader_outdoor, net, criterion, optimizer)
+    losses = train(train_loader, lable_loader, net, criterion, optimizer)
     #losses = test(test_loader, net, criterion)
     print(losses)
 
