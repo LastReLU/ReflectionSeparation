ab14de0668539f7f2e01cdfc512d574664409179
AndreyBocharnikov <a-bocharnikov@bk.ru>
2019-11-13 20:56:45 +0300

    constant dataset

--- train.py
+++ train.py
@@ -25,14 +25,6 @@ hyper_params = {
     'learning_rate': 0.01
 }
 
-def get_batch(batch):
-    features = batch[:, 0, :, :, :]
-    target_transpose = batch[:, 1, :, :, :]
-    target_reflection = batch[:, 2, :, :, :]
-    #target = th.Tensor(np.concatenate((target_transpose, target_reflection), axis=1))
-    return features, target_transpose, target_reflection
-
-
 def train(train_loader, model, criterion, optimizer, epochs=hyper_params['num_epochs'], save=True, device=th.device("cuda")):
     #with experiment.train():
     losses = []
@@ -43,37 +35,27 @@ def train(train_loader, model, criterion, optimizer, epochs=hyper_params['num_ep
         losses = []
         for i, batch in enumerate(train_loader):
             features, target_transmission = batch[:, :3, :, :], batch[:, 3:, :, :]
-            # print('featers and target', features[0, :, : , :], target_transmission[0, :, :, :])
-            #features = features.to(device)
-            #target_transmission = target_transmission.to(device)
             optimizer.zero_grad()
 
-            #predict_transmission, predict_reflection = model(features)
             predict_transmission = model(features)
             loss = criterion(predict_transmission, target_transmission)
 
-            """
-            Two head version
-            #loss2 = criterion(predict_reflection, target_reflection)
-            #loss = loss1 + loss2
-            #print("LOSSES: ", loss1, loss)
-            """
             loss.backward()
             optimizer.step()
             # print(epoch, step, loss.item())
 
-            #if (loss.item() > 1) :
-            # Image.fromarray((np.transpose(features[0].numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
-            # Image.fromarray((np.transpose(target_transmission[0].numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
-            # Image.fromarray((np.transpose(predict_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
-
+            """
+            #check strange picture
+            if (loss.item() > 1) :
+                Image.fromarray((np.transpose(features[0].numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
+                Image.fromarray((np.transpose(target_transmission[0].numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
+                Image.fromarray((np.transpose(predict_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype('uint8')).show()
+            """
             print('epoch number is ', epoch, '\n', "step number is ", step, '\n', 'loss ', loss.item(), '\n',
                   th.mean(model.conv_head1_1.weight.grad[0][0]), '\n', th.mean(model.conv_head1_2.weight.grad[0][0]), '\n',
                   th.mean(model.conv_head1_3.weight.grad[0][0]), '\n', th.mean(model.conv_head1_4.weight.grad[0][0]), '\n',
                   th.mean(model.conv_head1_5.weight.grad[0][0]), '\n', th.mean(model.conv_head1_6.weight.grad[0][0]))
             print()
-            #exit()
-            #experiment.log_metric('loss', loss.item(), step=step)
             step += 1
             losses.append(loss.item())
             if save:
@@ -84,20 +66,12 @@ def train(train_loader, model, criterion, optimizer, epochs=hyper_params['num_ep
 
 if __name__ == "__main__":
     print(th.__version__)
-    #experiment.log_parameters(hyper_params)
-    #device = th.device("cuda" if th.cuda().is_available() else "cpu")
-    #device = th.device("cuda")
-    #print(device)
-    #data = dtst.ImageDataSet(hyper_params['indoor_size'], hyper_params['outdoor_size'])
-    #train_loader = dtst.DataLoader(data, 1, 18)
-
     train_dataloader = DataLoader(cds.const_DataSet(test=False), batch_size=4)
 
     net = NetArticle()
     #net = th.load("weights2.hdf5")
     criterion = nn.MSELoss()
     optimizer = optim.Adam(net.parameters(), lr=hyper_params['learning_rate'])
-    #optimizer = optim.SGD(net.parameters(), lr=hyper_params['learning_rate'])
     losses = train(train_dataloader, net, criterion, optimizer)
     print(losses)
 
