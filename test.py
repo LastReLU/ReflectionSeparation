1066b0ef1b13cac2f2bc635b7447e24c78ee9669
root <sergevkim@yandex.ru>
2019-11-14 19:18:51 +0000

    checkpoint

--- test.py
+++ test.py
@@ -1,72 +1,33 @@
+import argparse
+from pathlib import Path
 import numpy as np
-import torch as th
-import torch.nn as nn
-import torch.nn.functional as F
+import torch
 import torch.optim as optim
-from model import NetArticle
-import Image_DataSet as dtst
-import cv2
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+import model
+import data
 
 
-hyper_params = {
-    'indoor_size': 20,
-    'outdoor_size': 100,
-    'input_size': (3, 128, 128),
-    'num_epochs': 10,
-    'learning_rate': 0.001
-}
-
-
-def get_batch(batch):
-    features = batch[:, 0, :, :, :]
-    target_transpose = batch[:, 1, :, :, :]
-    target_reflection = batch[:, 2, :, :, :]
-    #target = th.Tensor(np.concatenate((target_transpose, target_reflection), axis=1))
-    return features, target_transpose, target_reflection
-
-
-def test(test_loader, model, criterion):
-    losses = []
-    model.eval()
-    step = 0
-    for i, batch in enumerate(test_loader):
-        features, target_transmission, target_reflection = get_batch(batch)
-        predict_transmission, predict_reflection = model(features)
-        if i < 1:
-            print("-------------------------------")
-            a = (np.transpose(predict_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int)
-            b = (np.transpose(target_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int)
-            print(a - b)
-            print(predict_transmission[0] - target_transmission[0])
-
-            '''
-            writer = open("hello.txt", 'w')
-            for i in range(20, 30):
-                for j in range(20, 30):
-                    print(a[i][j][0], file=writer)
-            '''
-            print("-------------------------------")
-            #cv2.imwrite("imgs/target_trans" + str(i) + ".png", (np.transpose(target_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
-            #cv2.imwrite("imgs/transmission" + str(i) + ".png", (np.transpose(predict_transmission[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
-            #cv2.imwrite("imgs/reflection" + str(i) + ".png", (np.transpose(predict_reflection[0].detach().numpy(), (1, 2, 0)) * 255).astype(int))
-        loss1 = criterion(predict_transmission, target_transmission)
-        loss2 = criterion(predict_reflection, target_reflection)
-        loss = loss1 + loss2
-        losses.append(loss.item())
-        print(loss1.item())
-        step += 1
-    return losses
+def _parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('--logs', default='./runs/0')
+    p.add_argument('--batch_size', default=1, type=int)
+    p.add_argument('--epochs', default=1, type=int)
+    return p.parse_args()
 
 
 if __name__ == "__main__":
-    print(th.__version__)
 
-    data = dtst.ImageDataSet(hyper_params['indoor_size'], hyper_params['outdoor_size'])
-    test_loader = dtst.DataLoader(data, 1, 18, test=True)
+    # todo: split into train and test
+    # todo: make evaluation dataloaders
 
-    #net = NetArticle()
-    net = th.load("weights3.hdf5")
-    criterion = nn.MSELoss()
-    losses = test(test_loader, net, criterion)
-    print('####', losses)
-
+    #model = model.DummyModel().to(device)
+    model = torch.load('weights_new0.hdf5')
+    model.eval()
+    # let's fix the batch and iterate over it
+    image_batch = cv2.imread("difficult/")
+    batch = image_batch
+    info = model.compute_all(batch, device=device)
+    print(i, info)
